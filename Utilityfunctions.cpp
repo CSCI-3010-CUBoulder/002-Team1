766b1a444fed2319f7bf6dd40ea6c5011eeb6c41
Alex Burnley <albu5828@colorado.edu>
2020-02-11 11:14:38 -0700

    Utilityfunctions.h

---
+++ Utilityfunctions.cpp
@@ -0,0 +1,15 @@
+#include "Utilityfunctions.h"
+
+int Sign(int num) {
+	if(num < 0):
+		return -1;
+	return 1;
+}
+
+int Sum(std::vector<int> nums) {
+	int sum = 0;
+	for(int i = 0; i < nums.size(); i++) {
+		sum += nums[i];
+	}
+	return num;
+}
